3f47052abbe9c99fe4f3acfcda5c873efb0d03b8
Ilya Valasiuk <ilya.valasiuk@pargoo.com>
2023-03-31 15:50:57 +0300

    add copyright message to all files

--- src/api/indexes/types.ts
+++ src/api/indexes/types.ts
@@ -1,37 +1,53 @@
+/*
+ * Copyright (c) 2023 CryptoSlam, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export type IndexSummary = {
-	symbol: string;
-	name: string;
-	description: string;
-	bornDate: string;
-	startValue: number;
-	endValue: number;
-	change: number;
-	percentChange: number;
-	high: number;
-	low: number;
+  symbol: string;
+  name: string;
+  description: string;
+  bornDate: string;
+  startValue: number;
+  endValue: number;
+  change: number;
+  percentChange: number;
+  high: number;
+  low: number;
 };
 
 export type IndexHistory = {
-	symbol: string;
-	startTime: string;
-	endTime: string;
-	startValue: number;
-	endValue: number;
-	change: number;
-	percentChange: number;
+  symbol: string;
+  startTime: string;
+  endTime: string;
+  startValue: number;
+  endValue: number;
+  change: number;
+  percentChange: number;
 };
 
 export type IndexChanges = Record<
-	string,
-	{ increase: IndexChangeInfo[]; decrease: IndexChangeInfo[] }
+  string,
+  { increase: IndexChangeInfo[]; decrease: IndexChangeInfo[] }
 >;
 
 export type IndexChangeInfo = {
-	path: string;
-	name: string;
-	platformId: number;
-	platform: string;
-	platformIconUrl: string;
-	iconUrl: string;
-	change: number;
+  path: string;
+  name: string;
+  platformId: number;
+  platform: string;
+  platformIconUrl: string;
+  iconUrl: string;
+  change: number;
 };
--- src/errors/api-error/CustomApiError.ts
+++ src/errors/api-error/CustomApiError.ts
@@ -1,24 +1,40 @@
+/*
+ * Copyright (c) 2023 CryptoSlam, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export interface CustomApiError {
-	statusCode: number;
-	message: string;
-	details: string;
-	code: string;
+  statusCode: number;
+  message: string;
+  details: string;
+  code: string;
 }
 
 export class CustomApiError extends Error implements CustomApiError {
-	constructor(
-		statusCode: number,
-		message: string,
-		details = "",
-		// https://nodejs.org/docs/latest-v15.x/api/errors.html#nodejs-error-codes
-		code = ""
-	) {
-		super();
+  constructor(
+    statusCode: number,
+    message: string,
+    details = '',
+    // https://nodejs.org/docs/latest-v15.x/api/errors.html#nodejs-error-codes
+    code = ''
+  ) {
+    super();
 
-		this.statusCode = statusCode;
-		this.message = message;
-		this.details = details;
-		this.name = "Custom API error";
-		this.code = code;
-	}
+    this.statusCode = statusCode;
+    this.message = message;
+    this.details = details;
+    this.name = 'Custom API error';
+    this.code = code;
+  }
 }
