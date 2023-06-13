35d88b163e6fe485b3ed4ff50a7e34f519d217f3
aslaker <51129804+aslaker@users.noreply.github.com>
2023-06-13 12:23:17 -0500

    Remove csv export and match colors to fk labs site

--- src/components/IndexDetailed/IndexSummary/Summary/Summary.tsx
+++ src/components/IndexDetailed/IndexSummary/Summary/Summary.tsx
@@ -14,26 +14,24 @@
  * limitations under the License.
  */
 
-import React from 'react';
-import classNames from 'classnames';
-import { IndexSummary } from '../../../../api/indexes';
-import { formatLargeNumber } from '../../../../utils/formatting';
-import { useTheme } from '../../../../hooks/useTheme';
-import { Download } from '../../../common/ui/icons';
-import { Triangle } from '../../../../components/common/Triangle/Triangle';
-import { ContentLoader } from './ContentLoader/ContentLoader';
-import { PriceChange } from './PriceChange/PriceChange';
+import React from "react";
+import classNames from "classnames";
+import { IndexSummary } from "../../../../api/indexes";
+import { formatLargeNumber } from "../../../../utils/formatting";
+import { useTheme } from "../../../../hooks/useTheme";
+import { Download } from "../../../common/ui/icons";
+import { Triangle } from "../../../../components/common/Triangle/Triangle";
+import { ContentLoader } from "./ContentLoader/ContentLoader";
+import { PriceChange } from "./PriceChange/PriceChange";
 
 type Props = {
   data?: IndexSummary;
   isLoading: boolean;
-  onDownload: () => void;
 };
 
 export const Summary: React.FunctionComponent<Props> = ({
   data,
   isLoading,
-  onDownload,
 }) => {
   const { colors } = useTheme();
   if (isLoading) {
@@ -50,22 +48,22 @@ export const Summary: React.FunctionComponent<Props> = ({
     <div className="fkl-flex fkl-flex-row fkl-justify-between fkl-gap-4">
       <div
         className={classNames(
-          'fkl-flex fkl-flex-wrap fkl-items-center fkl-gap-2',
-          'sm:fkl-gap-4'
+          "fkl-flex fkl-flex-wrap fkl-items-center fkl-gap-2",
+          "sm:fkl-gap-4"
         )}
       >
         <div
           className={classNames(
-            'fkl-flex fkl-flex-row fkl-items-center fkl-gap-2',
-            'sm:fkl-gap-4'
+            "fkl-flex fkl-flex-row fkl-items-center fkl-gap-2",
+            "sm:fkl-gap-4"
           )}
         >
           <div
             style={{ color: colors.text }}
             className={classNames(
-              'fkl-text-[20px] fkl-leading-[24px] fkl-font-bold',
-              'sm:fkl-text-[24px] sm:fkl-leading-[28px]',
-              'lg:fkl-text-[56px] lg:fkl-leading-[64px]'
+              "fkl-text-[20px] fkl-leading-[24px] fkl-font-bold",
+              "sm:fkl-text-[24px] sm:fkl-leading-[28px]",
+              "lg:fkl-text-[56px] lg:fkl-leading-[64px]"
             )}
           >
             {formatLargeNumber(endValue, true)}
@@ -76,29 +74,29 @@ export const Summary: React.FunctionComponent<Props> = ({
 
         <div
           className={classNames(
-            'fkl-flex fkl-flex-row fkl-items-center fkl-gap-2',
-            'sm:fkl-gap-4'
+            "fkl-flex fkl-flex-row fkl-items-center fkl-gap-2",
+            "sm:fkl-gap-4"
           )}
         >
           <div className="fkl-flex fkl-flex-col">
             <div
               style={{ color: colors.text }}
               className={classNames(
-                'fkl-font-semibold fkl-text-[12px] fkl-leading-[16px]',
-                'sm:fkl-text-[14px] sm:fkl-leading-[18px]',
-                'lg:fkl-text-[16px] lg:fkl-leading-[24px]'
+                "fkl-font-semibold fkl-text-[12px] fkl-leading-[16px]",
+                "sm:fkl-text-[14px] sm:fkl-leading-[18px]",
+                "lg:fkl-text-[16px] lg:fkl-leading-[24px]"
               )}
             >
               HIGH
             </div>
             <div className="fkl-flex fkl-flex-row fkl-items-center fkl-gap-2">
-              <Triangle state={'positive'} />
+              <Triangle state={"positive"} />
               <div
                 style={{ color: colors.text }}
                 className={classNames(
-                  'fkl-text-[12px] fkl-leading-[16px]',
-                  'sm:fkl-text-[14px] sm:fkl-leading-[18px]',
-                  'lg:fkl-text-[16px] lg:fkl-leading-[24px]'
+                  "fkl-text-[12px] fkl-leading-[16px]",
+                  "sm:fkl-text-[14px] sm:fkl-leading-[18px]",
+                  "lg:fkl-text-[16px] lg:fkl-leading-[24px]"
                 )}
               >
                 {formatLargeNumber(high, true)}
@@ -109,46 +107,27 @@ export const Summary: React.FunctionComponent<Props> = ({
             <div
               style={{ color: colors.text }}
               className={classNames(
-                'fkl-font-semibold fkl-text-[12px] fkl-leading-[16px]',
-                'sm:fkl-text-[14px] sm:fkl-leading-[18px]',
-                'lg:fkl-text-[16px] lg:fkl-leading-[24px]'
+                "fkl-font-semibold fkl-text-[12px] fkl-leading-[16px]",
+                "sm:fkl-text-[14px] sm:fkl-leading-[18px]",
+                "lg:fkl-text-[16px] lg:fkl-leading-[24px]"
               )}
             >
               LOW
             </div>
             <div className="fkl-flex fkl-flex-row fkl-items-center fkl-gap-2">
-              <Triangle state={'negative'} />
+              <Triangle state={"negative"} />
               <div
                 style={{ color: colors.text }}
                 className={classNames(
-                  'fkl-text-[12px] fkl-leading-[16px]',
-                  'sm:fkl-text-[14px] sm:fkl-leading-[18px]',
-                  'lg:fkl-text-[16px] lg:fkl-leading-[24px]'
+                  "fkl-text-[12px] fkl-leading-[16px]",
+                  "sm:fkl-text-[14px] sm:fkl-leading-[18px]",
+                  "lg:fkl-text-[16px] lg:fkl-leading-[24px]"
                 )}
               >
                 {formatLargeNumber(low, true)}
               </div>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className="fkl-flex fkl-flex-row fkl-items-center fkl-justify-between fkl-gap-2">
-        <div
-          style={{ color: colors.text }}
-          className={classNames(
-            'fkl-hidden fkl-text-[16px] fkl-leading-[24px] fkl-font-bold',
-            'md:fkl-block'
-          )}
-        >
-          Download CSV
-        </div>
-        <div
-          style={{ color: colors.text }}
-          className="p-[5px] fkl-cursor-pointer"
-          onClick={onDownload}
-        >
-          <Download />
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: 'fkl-',
-  content: ['./src/**/*.{tsx,ts}'],
+  prefix: "fkl-",
+  content: ["./src/**/*.{tsx,ts}"],
   theme: {
     boxShadow: {
-      card: '-1.88313px 3.76627px 11.2988px rgba(0, 0, 0, 0.35)',
-      light: '1px 1px 12px -1px rgba(0, 0, 0, 0.12)',
-      menu: '-4px 5px 12px rgba(0, 0, 0, 0.25), -4px 4px 20px rgba(0, 0, 0, 0.25);',
+      card: "-1.88313px 3.76627px 11.2988px rgba(0, 0, 0, 0.35)",
+      light: "1px 1px 12px -1px rgba(0, 0, 0, 0.12)",
+      menu: "-4px 5px 12px rgba(0, 0, 0, 0.25), -4px 4px 20px rgba(0, 0, 0, 0.25);",
     },
     colors: {
-      transparent: 'transparent',
+      transparent: "transparent",
       black: {
-        DEFAULT: '#000',
-        light: 'rgba(0, 0, 0, 0.87)',
+        DEFAULT: "#000",
+        light: "rgba(0, 0, 0, 0.87)",
       },
       white: {
-        DEFAULT: '#fff',
+        DEFAULT: "#fff",
       },
       grey: {
-        DEFAULT: '#dadadf',
+        DEFAULT: "#dadadf",
       },
       green: {
-        DEFAULT: '#11BF42',
+        DEFAULT: "#44DE6F",
       },
       red: {
-        DEFAULT: '#E82B02',
+        DEFAULT: "#FD3939",
       },
       neutral: {
-        DEFAULT: '#8B7A98',
+        DEFAULT: "#8B7A98",
       },
       watermark: {
-        DEFAULT: 'rgb(226 226 226 / 20%)',
+        DEFAULT: "rgb(226 226 226 / 20%)",
       },
       primary: {
-        DEFAULT: '#063C94',
+        DEFAULT: "#063C94",
       },
       divider: {
-        DEFAULT: '#bbbbbb',
+        DEFAULT: "#bbbbbb",
       },
       tooltip: {
-        DEFAULT: 'rgba(97, 97, 97, 0.92)',
+        DEFAULT: "rgba(97, 97, 97, 0.92)",
       },
     },
     extend: {},
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [require("tailwind-scrollbar")],
 };
