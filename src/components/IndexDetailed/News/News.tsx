f00c8f14f574f9f604c1baa3f03e8df532a8547b
Ilya Valasiuk <ilya.valasiuk@pargoo.com>
2023-08-01 17:33:13 +0300

    remove unnecessary renders

--- src/components/IndexDetailed/News/News.tsx
+++ src/components/IndexDetailed/News/News.tsx
@@ -29,100 +29,103 @@ type Props = {
 
 const HEADER_OFFSET = 53;
 
-export const News: React.FC<Props> = ({
-  symbol,
-  timeState,
-  clickedDatePoint,
-}) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const { articlesByDay: news, isLoading } = useArticlesWithMarkers({
-    symbol,
-    timeState,
-    isNewsEnabled: true,
-  });
-  const { colors } = useTheme();
-  const isEmpty = !isLoading && Object.keys(news).length === 0;
+export const News: React.FC<Props> = React.memo(
+  ({ symbol, timeState, clickedDatePoint }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const { articlesByDay: news, isLoading } = useArticlesWithMarkers({
+      symbol,
+      timeState,
+      isNewsEnabled: true,
+    });
+    const { colors } = useTheme();
+    const isEmpty = !isLoading && Object.keys(news).length === 0;
 
-  useEffect(() => {
-    if (clickedDatePoint) {
-      const elementToScroll = ref?.current?.querySelector<HTMLDivElement>(
-        `[data-point='${clickedDatePoint}']`
-      );
+    useEffect(() => {
+      if (clickedDatePoint) {
+        const elementToScroll = ref?.current?.querySelector<HTMLDivElement>(
+          `[data-point='${clickedDatePoint}']`
+        );
 
-      if (elementToScroll) {
-        ref?.current?.scrollTo({
-          top: elementToScroll.offsetTop - HEADER_OFFSET,
-          behavior: "smooth",
-        });
+        if (elementToScroll) {
+          ref?.current?.scrollTo({
+            top: elementToScroll.offsetTop - HEADER_OFFSET,
+            behavior: "smooth",
+          });
+        }
       }
-    }
-  }, [ref, clickedDatePoint]);
+    }, [ref, clickedDatePoint]);
 
-  return (
-    <div
-      ref={ref}
-      className="fkl-px-5 fkl-flex fkl-flex-col fkl-gap-2 fkl-overflow-scroll fkl-h-full fkl-relative"
-      style={{ color: colors.text }}
-    >
+    return (
       <div
-        className="fkl-sticky fkl-top-0 fkl-z-10"
-        style={{ backgroundColor: colors.background }}
+        ref={ref}
+        className="fkl-px-5 fkl-flex fkl-flex-col fkl-gap-2 fkl-overflow-scroll fkl-h-full fkl-relative"
+        style={{ color: colors.text }}
       >
-        <div className="fkl-text-[24px] fkl-mb-2">Market News & Insights</div>
-        <hr />
-      </div>
-
-      {isLoading ? (
-        <div className="fkl-flex fkl-items-center fkl-justify-center fkl-flex-1">
-          <Loader />
+        <div
+          className="fkl-sticky fkl-top-0 fkl-z-10"
+          style={{ backgroundColor: colors.background }}
+        >
+          <div className="fkl-text-[24px] fkl-mb-2">Market News & Insights</div>
+          <hr />
         </div>
-      ) : isEmpty ? (
-        <div className="fkl-flex fkl-items-center fkl-justify-center fkl-flex-1">
-          <div>No news for selected time state</div>
-        </div>
-      ) : (
-        Object.entries(news).map(([datepoint, artiles]) => (
-          <div key={datepoint} data-point={datepoint}>
-            <div>{dayjs(datepoint).format("MMM DD, YYYY")}</div>
 
-            <div className="fkl-flex fkl-flex-col fkl-gap-6 fkl-py-5">
-              {artiles?.map((article) => {
-                const articleImage = article._embedded["wp:featuredmedia"]?.[0];
-
-                return (
-                  <a
-                    key={article.id}
-                    href={article.link}
-                    className="fkl-flex fkl-flex-row fkl-gap-4 hover:fkl-text-[#6842FF]"
-                    target="_blank"
-                  >
-                    <div className="fkl-w-[100px] fkl-h-[60px] fkl-relative fkl-shrink-0 fkl-overflow-hidden">
-                      <img
-                        src={
-                          articleImage?.media_details?.sizes?.medium
-                            ?.source_url ?? articleImage?.source_url
-                        }
-                        alt="article picture"
-                        className="fkl-absolute fkl-left-0 fkl-top-0 fkl-right-0 fkl-bottom-0"
-                      />
-                    </div>
-                    <div className="fkl-flex  fkl-flex-col fkl-justify-between">
-                      <div className="fkl-text-xs">
-                        {article.title.rendered}
-                      </div>
-                      <div className="fkl-text-[10px]">
-                        {dayjs(article.date).format(
-                          "MMM DD[,] YYYY [|] hh:mma"
-                        )}
-                      </div>
-                    </div>
-                  </a>
-                );
-              })}
-            </div>
+        {isLoading ? (
+          <div className="fkl-flex fkl-items-center fkl-justify-center fkl-flex-1">
+            <Loader />
           </div>
-        ))
-      )}
-    </div>
-  );
-};
+        ) : isEmpty ? (
+          <div className="fkl-flex fkl-items-center fkl-justify-center fkl-flex-1">
+            <div>No news for selected time state</div>
+          </div>
+        ) : (
+          Object.entries(news).map(([datepoint, artiles]) => (
+            <div key={datepoint} data-point={datepoint}>
+              <div>{dayjs(datepoint).format("MMM DD, YYYY")}</div>
+
+              <div className="fkl-flex fkl-flex-col fkl-gap-6 fkl-py-5">
+                {artiles?.map((article) => {
+                  const articleImage =
+                    article._embedded["wp:featuredmedia"]?.[0];
+
+                  return (
+                    <a
+                      key={article.id}
+                      href={article.link}
+                      className="fkl-flex fkl-flex-row fkl-gap-4 hover:fkl-text-[#6842FF]"
+                      target="_blank"
+                    >
+                      <div className="fkl-w-[100px] fkl-h-[60px] fkl-relative fkl-shrink-0 fkl-overflow-hidden">
+                        <img
+                          src={
+                            articleImage?.media_details?.sizes?.medium
+                              ?.source_url ?? articleImage?.source_url
+                          }
+                          alt="Article Picture"
+                          className="fkl-absolute fkl-left-0 fkl-top-0 fkl-right-0 fkl-bottom-0"
+                        />
+                      </div>
+                      <div className="fkl-flex  fkl-flex-col fkl-justify-between">
+                        <div
+                          className="fkl-text-xs"
+                          dangerouslySetInnerHTML={{
+                            __html: article.title.rendered,
+                          }}
+                        />
+
+                        <div className="fkl-text-[10px]">
+                          {dayjs(article.date).format(
+                            "MMM DD[,] YYYY [|] hh:mma"
+                          )}
+                        </div>
+                      </div>
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    );
+  }
+);
